a816f5aae6ca2316c35bd556f68bab0235df9a2f
Seike Takumi <takoforest+github@gmail.com>
2014-06-09 02:33:54 +0900

    update

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
 $(function(){
-  var daysInMonth = function(year, month){
-    var firstDay = new Date(year, month - 1);
-    var lastDay  = new Date(year, month);
-    var dayCount = (lastDay - firstDay) / (86400 * 1000);
-    var dateFormatter = (function(y, m){
+  App = {};
+  App.Utils = {
+    DateFormatYmd: function(y, m){
       var yyyy = ('000' + y).slice(-4);
       var MM   = ('0' + m) .slice(-2);
       return function(day){
         var dd = ('0' + day).slice(-2);
         return yyyy + MM + dd;
       };
-    })(year, month);
-    var days = _.map(_.range(1, dayCount+1), dateFormatter);
-    
+    }
   };
+  var daysInMonth = function(year, month){
+        // 今月の日数を計算
+    var firstDay = new Date(year, month - 1);
+    var lastDay  = new Date(year, month);
+    var dayCount = (lastDay - firstDay) / (86400 * 1000);
+    // yyyyMMdd型で該当月の日のリストを作成
+    return _.map(_.range(1, dayCount+1), App.Utils.DateFormatYmd(year, month));
 
-  var dateFormat = function(year, month ,day){
-    return ('000'+ year).slice(-4) + ('0' + month).slice(-2) + ('0' + day).slice(-2);
   };
-
+  
   var days = daysInMonth(2014,6);
+  // bodyに表示
+  $(document.body).empty();
+  var template = _.template('<span class="cell" data-date="<%-ymd%>"><%-day%></span>');
+  _.each(days, function(ymd){
+    var param = {
+      'ymd': ymd,
+      'day': ymd.slice(-2)
+    };
+    var cell = $(template(param));
+    if(new Date(ymd.slice(0,4), parseInt(ymd.slice(4,6)) - 1, ymd.slice(6,8)).getDay() == 0){
+      cell.addClass('first');
+    }
+    $(document.body).append(cell);
+  });
+  
 });

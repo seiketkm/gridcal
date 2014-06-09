4ef5de62665d33485c80e7bf8f62c901007b55bb
Seike Takumi <takoforest+github@gmail.com>
2014-06-10 01:49:39 +0900

    update

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-$(function(){
-  App = {};
-  App.Utils = {
+App = {
+  Utils : {
     DateFormatYmd: function(y, m){
       var yyyy = ('000' + y).slice(-4);
       var MM   = ('0' + m) .slice(-2);
@@ -8,19 +7,24 @@ $(function(){
         var dd = ('0' + day).slice(-2);
         return yyyy + MM + dd;
       };
+    },
+    CountDaysInMonth: function(year, month){
+      // 今月の日数を計算
+      var firstDay = new Date(year, month - 1);
+      var lastDay  = new Date(year, month);
+      return (lastDay - firstDay) / (86400 * 1000);
+    },
+    createDaysInMonth: function(year, month){
+      var dayCount =  App.Utils.CountDaysInMonth(year, month);
+      // yyyyMMdd型で該当月の日のリストを作成
+      return _.map(_.range(1, dayCount+1), App.Utils.DateFormatYmd(year, month));
     }
-  };
-  var daysInMonth = function(year, month){
-        // 今月の日数を計算
-    var firstDay = new Date(year, month - 1);
-    var lastDay  = new Date(year, month);
-    var dayCount = (lastDay - firstDay) / (86400 * 1000);
-    // yyyyMMdd型で該当月の日のリストを作成
-    return _.map(_.range(1, dayCount+1), App.Utils.DateFormatYmd(year, month));
+  }
+};
 
-  };
-  
-  var days = daysInMonth(2014,6);
+
+$(function(){  
+  var days = App.Utils.createDaysInMonth(2014,6);
   // bodyに表示
   $(document.body).empty();
   var template = _.template('<span class="cell" data-date="<%-ymd%>"><%-day%></span>');
@@ -35,5 +39,9 @@ $(function(){
     }
     $(document.body).append(cell);
   });
-  
+
+  $(document).on('mousedown touch', '.cell', function(e){
+    console.log($(this).data('date'));
+    $(this).toggleClass('selected');
+  });
 });
